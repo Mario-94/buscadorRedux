e63ce72768a2228eb9ff8611b4cbb4edac72b6ee
AuxSistemas <Auxsistemas>
2023-01-27 19:49:34 -0600

    Empezamos el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Buscador from './components/Buscador';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <div className="row">
+          <div className="">
+            <div className="card mt-5" style={{ maxWidth: '470px' }}>
+              <div className="row no-gutters">
+                <div className="col-12">
+                  <div className="card-body">
+                    <div className="card-title h4 text-center">
+                      <Buscador />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </Provider>
   );
 }
 
