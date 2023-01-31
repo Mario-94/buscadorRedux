cd8376ee39bec86126afc8105d0903df20485ecd
AuxSistemas <Auxsistemas>
2023-01-30 19:55:02 -0600

    Mepezamos lo de un nuevo curso

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import Buscador from './components/Buscador';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
- 
+
+
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <div className="row">
-          <div className="">
-            <div className="card mt-5" style={{ maxWidth: '470px' }}>
-              <div className="row no-gutters">
-                <div className="col-12">
-                  <div className="card-body">
-                    <div className="card-title h4 text-center">
-                      <Buscador />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-       
-      </div>
-    </Provider>
+    <>
+
+
+    </>
   );
 }
 
